2f9f2fbfa96574330edd66853bf750f5c7f24aec
Archie Jaskowicz <archie@archies-mini.home>
2023-11-12 13:23:13 +0000

    feat: added all files (not sure if works)

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# build by specifying architecture on command line:
+#
+# ./build.sh arm
+# ./build.sh arm64
+#
+docker build --build-arg ARCH=$1 RUNNER_VERSION=2.311.0 --tag docker-github-runner-osx .
